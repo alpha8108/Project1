134b251c2a0f8e405a7e402529884c1880e6e916
JeongGeun <alpha8108@naver.com>
2024-01-18 14:24:19 +0900

    두번째 커밋 빼기 기능 구현함 ~~~~

--- Untitled.py
+++ Untitled.py
@@ -1,6 +1,18 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+## 함수 선언부
+def add_func(n1, n2):
+    result = n1 + n2
+    return result
+
+def sub_func(n1, n2):
+    return n1 - n2
+
+## 전역 변수부
+num1, num2 = 100, 200
+res = 0
+
+## 메인 코드부
+res = add_func(num1, num2)
+print(num1,'+',num2,'=',res)
+
+res = add_func(num1, num2)
+print(num1,'-',num2,'=',res)
